1cddc3af09a5c3b3c065ecd0171deb357fe30d92
26rowandavis-sys <26.rowan.davis@carolinaday.org>
2026-01-09 00:45:53 +0000

    next

--- Tuesday.py
+++ Tuesday.py
@@ -1,1 +1,17 @@
-print("Strawberry")
+print("triceratops")
+
+schoolName = "CDS"
+print( "Welcome to " + schoolName + ".") #concatenation
+
+#example on input below
+yourName = input ()
+
+
+#program for today
+print ("nice to meet you, " + yourName)
+place = input ("where did you go? ")
+movie = input ("what did you watch? ")
+book = input ("what book did you read? ")
+funFact = input ("tell me a fun fact ")
+print ("Rowan went to " + place + ".Rowan watched " + movie + ".Rowan read " + book + ".did you know" + funFact)
+
---
+++ integer-practice.py
@@ -0,0 +1,11 @@
+intVar = input ("13 ") 
+Fint = input ("1.2 ")
+string = input ("Math is annoying ")
+print ("intVar")
+print ("Fint") 
+print ("string ")
+string = input ("math can be useful though ")
+Fint = input ("2.5 ")
+print ("Math can be useful " + ".Fint is 2.5 ")
+
+       
