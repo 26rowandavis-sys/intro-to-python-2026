f8290da683ff7d42963d2dc2c95aff9afcd5dfec
26rowandavis-sys <26.rowan.davis@carolinaday.org>
2026-01-09 15:57:16 +0000

    final

--- integer-practice.py
+++ integer-practice.py
@@ -1,11 +1,12 @@
-intVar = input ("13 ") 
-Fint = input ("1.2 ")
-string = input ("Math is annoying ")
-print ("intVar")
-print ("Fint") 
-print ("string ")
+intVar = 13 
+Fint = 1.2 
+string = "Math is annoying "
+print (intVar)
+print (Fint) 
+print (string )
 string = input ("math can be useful though ")
 Fint = input ("2.5 ")
-print ("Math can be useful " + ".Fint is 2.5 ")
+print ("Math can be useful ")
+print ( "Fint is 2.5 ")
 
        
